fc4578a80bad6d0fe76f6686172aeb784b5bbf62
Jahanzeb Naeem <naeemjahanzeb@gmail.com>
2023-06-12 02:52:57 +0500

    external version

--- SignChatter-WebApp/vite-project/components/Spacer.jsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-const Spacer = styled.div`
-  width: ${(props) => (props.width ? props.width : "auto")};
-  height: ${(props) => (props.height ? props.height : "auto")};
-`;
-export { Spacer };
